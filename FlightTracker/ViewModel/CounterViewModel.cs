39a020fc14808b9f3bbc7a33fe5d4e2f8829ad8a
Minardi299 <phu.ngo@outlook.com>
2024-09-02 23:15:19 -0400

    set up mvvm pattern

---
+++ FlightTracker/ViewModel/CounterViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.ObjectModel;
+
+
+namespace FlightTracker.ViewModel
+{
+    public class CounterViewModel
+    {
+
+        private int _currentCount;
+        public int CurrentCount
+        {
+            get => _currentCount;
+            set
+            {
+                _currentCount = value;
+
+            }
+        }
+        public int Amount { get; set; } = 1;
+        public CounterViewModel(int count)
+        {
+            this.CurrentCount = count;
+        }
+        public void IncrementCount()
+        {
+            CurrentCount += Amount;
+        }
+
+    }
+}
