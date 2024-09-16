9631d9f3b5b85d13438619bd3b1cce9a5b282005
Minardi299 <phu.ngo@outlook.com>
2024-09-15 22:47:41 -0400

    get api call up and running

---
+++ FlightTracker/Model/Flight.cs
@@ -0,0 +1,6 @@
+﻿namespace FlightTracker.Model
+{
+    public class Flight
+    {
+    }
+}
---
+++ FlightTracker/ViewModel/FlightViewModel.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace FlightTracker.ViewModel
+{
+    public class FlightViewModel
+    {
+        private static readonly HttpClient client = new HttpClient();
+        private readonly string baseURL = "https://api.aviationstack.com/v1/flights";
+        private readonly string accessKey  ;
+        public string FlightId { get; set; }
+       
+        
+        public FlightViewModel(string FlightId)
+        {
+            
+            this.FlightId = FlightId;
+            this.accessKey = Environment.GetEnvironmentVariable("AviationStackAPIKey");
+            if (string.IsNullOrEmpty(accessKey))
+            {
+                throw new InvalidOperationException("AviationStackAPIKey is not set in environment variables.");
+            }
+        }
+        private string BuildApiUrl()
+        {
+            
+            string queryString = $"?access_key={accessKey}";
+
+            if (!string.IsNullOrEmpty(FlightId))
+            {
+                queryString += $"&flight_id={FlightId}";
+            }
+
+            
+            return baseURL + queryString;
+        }
+        public  async Task<string> GetApiData()
+        {
+            string apiUrl = BuildApiUrl();
+
+            // Sending the GET request
+            HttpResponseMessage response = await client.GetAsync(apiUrl);
+
+            // Ensure the request was successful
+            response.EnsureSuccessStatusCode();
+
+            // Read the response content as a string
+            string responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody;
+        }
+    }
+}
