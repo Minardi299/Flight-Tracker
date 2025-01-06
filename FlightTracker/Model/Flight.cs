99bc9344b90b5a79e6f858d73890879551e55fe5
Minardi299 <phu.ngo@outlook.com>
2025-01-06 15:43:26 -0500

    make propeties nullable

--- FlightTracker/Model/Flight.cs
+++ FlightTracker/Model/Flight.cs
@@ -8,37 +8,37 @@ namespace FlightTracker.Model
         [JsonProperty("flight")]
         private JObject FlightData { get; set; }
         [JsonIgnore]
-        public string Number => FlightData?["number"]?.ToString();
+        public string? Number => FlightData?["number"]?.ToString();
 
-        public string Iata => FlightData?["iata"]?.ToString();
+        public string? Iata => FlightData?["iata"]?.ToString();
 
-        public string Icao => FlightData?["icao"]?.ToString();
+        public string? Icao => FlightData?["icao"]?.ToString();
 
         public object CodeShared => FlightData?["codeshared"]?.ToString();
 
         [JsonProperty("departure")]
-        public Airport Departure { get; set; }
+        public Airport? Departure { get; set; }
 
         [JsonProperty("arrival")]
-        public Airport Arrival { get; set; }
+        public Airport? Arrival { get; set; }
 
         [JsonProperty("airline")]
-        public Airline Airline { get; set; }
+        public Airline? Airline { get; set; }
 
         [JsonProperty("aircraft")]
-        private JObject AircraftData { get; set; }
+        private JObject? AircraftData { get; set; }
 
         // Create a property to get just the 'iata' code
         [JsonIgnore]
-        public string AircraftIata => AircraftData?["iata"]?.ToString();
+        public string? AircraftIata => AircraftData?["iata"]?.ToString();
         [JsonProperty("flight_date")]
-        public DateOnly Flight_date { get; set; }
+        public DateOnly? Flight_date { get; set; }
 
         [JsonProperty("flight_status")]
-        public string Flight_status { get; set; }
+        public string? Flight_status { get; set; }
         public  override string ToString()
         {
-            return $"{this.Number} is the flight number and airline is {this.Airline.Name}";
+            return $"{this?.Number} is the flight number and airline is {this.Airline.Name}";
         }
     }
 }
