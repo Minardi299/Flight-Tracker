6e4d60d9eac0a45317115643e86e22519462d65f
Minardi299 <phu.ngo@outlook.com>
2025-01-06 15:18:29 -0500

    add airport object

--- FlightTracker/Model/Airport.cs
+++ FlightTracker/Model/Airport.cs
@@ -4,5 +4,32 @@ namespace FlightTracker.Model
 {
     public class Airport
     {
+        [JsonProperty("airport")]
+        public string? Name { get; set; }
+        [JsonProperty("timezone")]
+        public string? TimeZone { get; set; }
+        [JsonProperty("iata")]
+        public string? AirportIata { get; set; }
+        [JsonProperty("icao")]
+        public string? AirportIcao { get; set; }
+        [JsonProperty("terminal")]
+        public string? Terminal { get; set; }
+        [JsonProperty("gate")]
+        public string? Gate{ get; set; }
+        [JsonProperty("delay")]
+        public int? Delay { get; set; }
+        [JsonProperty("scheduled")]
+        public DateTime? Scheduled { get; set; }
+
+        [JsonProperty("estimated")]
+        public DateTime? Estimated { get; set; }
+        [JsonProperty("actual")]
+        public DateTime? Actual { get; set; }
+
+        [JsonProperty("estimated_runway")]
+        public DateTime? EstimatedRunway { get; set; }
+
+        [JsonProperty("actual_runway")]
+        public DateTime? ActualRunway { get; set; }
     }
 }
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # Flight-Tracker
+api to consider:
+https://airlabs.co/
+https://aviationstack.com/
+https://adsb.lol/
+https://www.reddit.com/r/ADSB/comments/17n3vq3/any_free_adsb_api/
+https://www.kaggle.com/datasets/matinsajadi/flights
+
+https://www.reddit.com/r/selfhosted/comments/1fe2pq7/introducing_airtrail_a_personal_flight_tracking/
+
+example code
+https://www.flightaware.com/commercial/aeroapi/
